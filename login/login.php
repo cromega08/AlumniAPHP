f47a324c4af75d461bd4902007b55b46addf3740
cromega <cr.jrg08@gmail.com>
2023-03-09 11:47:36 -0500

    starting project

---
+++ login/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>AlumniAPHP - Login</title>
+</head>
+
+<header>
+	<h1>
+		Login
+	</h1>
+</header>
+
+<body>
+	<nav class="nav nav-tabs">
+		<button class="nav-item">Sign In</button>
+		<button class="nav-item">Sign Up</button>
+	</nav>
+</body>
+
+<footer>
+
+</footer>
+
+</html>
